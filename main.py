dd1641e95cf6016e5ac23d5a848e351c9436f80b
trick491 <143121688+trick491@users.noreply.github.com>
2025-04-09 16:24:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,40 @@ def fib_recursive(n, counts):
     """    
     counts[n] += 1
     ###TODO
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
     
 
-    
 def fib_top_down(n, fibs):
     ###TODO
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    if fibs[n] != -1:
+        return fibs[n]
+
+    fibs[n] = fib_top_down(n - 1, fibs) + fib_top_down(n - 2, fibs)
+    return fibs[n]
 
 
 def fib_bottom_up(n):
     ###TODO
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
 
+    fibs = [0] * (n + 1)
+    fibs[1] = 1
 
+    for i in range(2, n + 1):
+        fibs[i] = fibs[i - 1] + fibs[i - 2]
 
+    return fibs[n]
 
